1ff8e03aaeba82a69104558aa622fc616a7c8265
Aylee <no.power.in.the.verse.can.stop.me@gmail.com>
2017-06-11 15:52:20 +0300

    My Django Girls app, first commit

---
+++ example1.py
@@ -0,0 +1,29 @@
+name = "Aylee"
+if name == "Aylee":
+	print("Hey Aylee!")
+elif name == "Tom":
+	print("Hey Tom!")
+else:
+	print("Hey gorgeous!")
+#4r5rd5t
+def Tom():
+	print("Good morning")
+	print ("My spider senses are tingling")
+Tom()
+def h1(name):
+	if name == "Ayellet":
+		print("Hi Ayellet ;c)")
+	elif name == "Flora":
+		print("Hi Flora ;c)")
+	else:
+		print("Hi Libertine!")
+#hi("Ayellet")
+def Hey(name):
+	print("Hey " + name + " and welcome to my bunk!")
+Hey("everybody")
+Wishlist = ["Emma Stone", "Tom Holland", "Ezra Miller", "Caleb Landry Jones", "Melech Zilbershlag"]
+for name in Wishlist:
+	Hey(name)
+	print("Next object")
+for i in range(4, 14):
+	print(i)
